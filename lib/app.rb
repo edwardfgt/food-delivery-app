ccadad2c05cf70f3c621ab264dc7fec3e51368ff
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-01-26 12:12:35 +0000

    add send_sms method and logic using Twilio API

--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,14 @@
 require_relative "./menu"
 require_relative "./dish"
+require "twilio-ruby"
+require_relative "../credentials"
 
 class App
   def initialize(menu)
     @menu = menu
     @cart = []
+    @account_sid = API_credentials::ACCOUNT_SID
+    @auth_token = API_credentials::AUTH_TOKEN
   end
   
   def view_menu
@@ -20,10 +24,19 @@ class App
   end
   
   def check_out
+    send_sms
     return @menu.checkout(@cart)
   end
   
   def send_sms
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+    @client.messages.create(
+      from: '+16064044821',
+      to: '+447497163596',
+      body: 'TEST'
+    )
+    puts "SMS SENT"
+    
   end
 end
 
@@ -36,4 +49,4 @@ menu.add_food(dish2)
 app.add_cart("salami")
 app.add_cart("olives")
 app.add_cart("salami")
-puts app.check_out
+puts app.send_sms
